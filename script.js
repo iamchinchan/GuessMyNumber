1a69e843bdfee2145f8990b09914ddf801c01af8
Jatin Goyal <goyaljatin344@gmail.com>
2022-12-16 22:49:40 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+// const sd = 'y';
+// const newFnAdd = sd ? (a, b) => a + b : 'not done yet';
+// const newFnAdd = sd
+//   ? function addvar(a, b) {
+//       return a + b;
+//     }
+//   : 'not done yet';
+// console.log(newFnAdd);
+// const addVal = newFnAdd(15, 2);
+// console.log(addVal);
+
+// document.querySelector('.check').addEventListener('click', () => {
+//   const guessVal = document.querySelector('.guess').value;
+//   console.log('guess value is: ', guessVal);
+// });
+
+// document
+//   .querySelector('.check')
+//   .addEventListener('click', function getGuessVal() {
+//     const guessVal = document.querySelector('.guess').value;
+//     console.log('guess value is: ', guessVal);
+//   });
+
+// function getGuessVal() {
+//   const guessVal = document.querySelector('.guess').value;
+//   console.log('guess value is: ', guessVal);
+// }
+// document.querySelector('.check').addEventListener('click', getGuessVal);
+
+// const getGuessVal = function () {
+//   const guessVal = document.querySelector('.guess').value;
+//   console.log('guess value is: ', guessVal);
+// };
+// document.querySelector('.check').addEventListener('click', getGuessVal);
+
+// const getGuessVal = () => {
+//   const guessVal = document.querySelector('.guess').value;
+//   console.log('guess value is: ', guessVal);
+// };
+// document.querySelector('.check').addEventListener('click', getGuessVal);
+
+// document.querySelector('.check').addEventListener('click', function () {
+//   const guessVal = document.querySelector('.guess').value;
+//   console.log('guess value is: ', guessVal);
+// });
